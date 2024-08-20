cfa088aaef5411eae33eb5126644eb3ada51ea32
Dominik <d.jagodzky@gmail.com>
2024-08-20 14:50:21 +0200

    Env_setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
     return (
-        <div className='App'>
+        <div className={styles.App}>
 
-            <nav>nav - logo goes here only</nav>
-            <header>header - search box and search button - search form goes here</header>
-            <main>Results list and user selection 2 columns go here</main>
-            <footer>logo only will go here as a footer</footer>
+            <nav>
+                <h2>logo only in navbar</h2>
+                <img src='#' alt='img'/>
+            </nav>
+            <header>
+                <h2>header - search box and search button - search form goes here</h2>
+                <input placeholder='Enter Search query' />
+                <button>Submit search query</button>
+            </header>
+            <main>
+                <div className={styles.column1}>
+                    <h2>Column with Search results</h2>
+                </div>
+                <div className={styles.column2}>
+                    <h2>Column with items added by the user with the option to create playlist</h2>
+                    <button>Save playlist</button>
+                </div>
+            </main>
+            <footer>
+                <h2>logo only will go here as a footer</h2>
+            </footer>
 
         </div>
     );
